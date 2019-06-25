56db9d5687da73bddba11d934e3f62d6d0f46648
07x8 <codewars.nl@gmail.com>
2019-06-25 20:10:29 +0200

    Updated readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.MD", "r") as f:
 
 setuptools.setup(
     name="codewars", # codewars when updating to pip
-    version="0.0.2c",
+    version="0.0.3",
     author="Annihilator708",
     author_email="",
     description="A toolbox for daily use. This is just a toolbox to make my life easier. Maybe yours too..",
